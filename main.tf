d88dda8bceeb900b861238e347852692519bc428
Anh Quach <tuananh.quach@moca.vn>
2022-06-28 12:34:38 +0700

    Init commit

---
+++ main.tf
@@ -0,0 +1,137 @@
+###########################################################
+# LOAD BALANCER CONTROLLER
+###########################################################
+resource "helm_release" "aws_loadbalancer_controller" {
+  count            = var.enable_aws_lb_controller ? 1 : 0
+  name             = "aws-load-balancer-controller"
+  namespace        = "kube-system"
+  create_namespace = true
+  repository       = "https://aws.github.io/eks-charts"
+  chart            = "aws-load-balancer-controller"
+
+  dynamic "set" {
+    iterator = each_item
+    for_each = try(var.aws_lb_controller_context, {})
+
+    content {
+      name  = each_item.key
+      value = each_item.value
+    }
+  }
+}
+
+###########################################################
+# INGRESS NGINX
+###########################################################
+resource "helm_release" "ingress_nginx" {
+  count            = var.enable_ingress_nginx ? 1 : 0
+  name             = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+
+  values = [
+    file("${path.module}/ingress-nginx/values-custom.yaml")
+  ]
+
+  dynamic "set" {
+    iterator = each_item
+    for_each = try(var.aws_lb_controller_context, {})
+
+    content {
+      name  = each_item.key
+      value = each_item.value
+    }
+  }
+}
+
+###########################################################
+# CERT MANAGER
+###########################################################
+resource "helm_release" "cert_manager" {
+  count            = var.enable_cert_manager ? 1 : 0
+  name             = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+
+  values = [
+    file("${path.module}/cert-manager/values-custom.yaml")
+  ]
+
+  dynamic "set" {
+    iterator = each_item
+    for_each = try(var.aws_cert_manager_context, {})
+
+    content {
+      name  = each_item.key
+      value = each_item.value
+    }
+  }
+}
+resource "kubectl_manifest" "cluster_issuer" {
+  count     = var.enable_cert_manager ? 1 : 0
+  yaml_body = file("${path.module}/cert-manager/cluster-issuer.yaml")
+
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
+
+###########################################################
+# EXTERNAL DNS
+###########################################################
+resource "helm_release" "external_dns" {
+  count            = var.enable_external_dns ? 1 : 0
+  name             = "external-dns"
+  namespace        = "external-dns"
+  create_namespace = true
+  repository       = "https://kubernetes-sigs.github.io/external-dns"
+  chart            = "external-dns"
+
+  values = [
+    file("${path.module}/external-dns/values-custom.yaml")
+  ]
+
+  dynamic "set" {
+    iterator = each_item
+    for_each = try(var.aws_cert_manager_context, {})
+
+    content {
+      name  = each_item.key
+      value = each_item.value
+    }
+  }
+}
+
+###########################################################
+# JENKINS
+###########################################################
+resource "helm_release" "jenkins" {
+  count            = var.enable_jenkins ? 1 : 0
+  name             = "jenkins"
+  namespace        = "jenkins"
+  create_namespace = true
+  repository       = "https://charts.jenkins.io"
+  chart            = "jenkins"
+
+  values = [
+    file("${path.module}/jenkins/values-custom.yaml")
+  ]
+
+  dynamic "set" {
+    iterator = each_item
+    for_each = try(var.aws_cert_manager_context, {})
+
+    content {
+      name  = each_item.key
+      value = each_item.value
+    }
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "enable_aws_lb_controller" {
+  description = "Enable AWS LB Controller or not"
+  type        = bool
+  default     = false
+}
+
+variable "aws_lb_controller_context" {
+  description = "Set option for AWS LB Controller"
+  default     = {}
+}
+
+variable "enable_ingress_nginx" {
+  description = "Enable Ingress Nginx or not"
+  type        = bool
+  default     = false
+}
+
+variable "aws_ingress_nginx_context" {
+  description = "Set option for Ingress Nginx"
+  default     = {}
+}
+
+variable "enable_cert_manager" {
+  description = "Enable Cert Manager or not"
+  type        = bool
+  default     = false
+}
+
+variable "aws_cert_manager_context" {
+  description = "Set option for Cert Manager"
+  default     = {}
+}
+
+variable "enable_external_dns" {
+  description = "Enable External DNS or not"
+  type        = bool
+  default     = false
+}
+
+variable "aws_external_dns_context" {
+  description = "Set option for External DNS"
+  default     = {}
+}
+
+variable "enable_jenkins" {
+  description = "Enable Jenkins or not"
+  type        = bool
+  default     = false
+}
+
+variable "aws_jenkins_context" {
+  description = "Set option for Jenkins"
+  default     = {}
+}
