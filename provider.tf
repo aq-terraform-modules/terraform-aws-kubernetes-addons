e309d3ad8277e8146efde4e0c0d2ba0ea37144ba
Anh Quach <tuananh.quach@moca.vn>
2022-06-28 12:52:14 +0700

    Delete unnecessary depends_on

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.5.1"
-    }
-
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
