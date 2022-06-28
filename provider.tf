71ea56138b1d8c0644381f987700bf5d76a0ed94
Anh Quach <tuananh.quach@moca.vn>
2022-06-28 12:37:58 +0700

    Update providers

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.1"
+    }
+
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
