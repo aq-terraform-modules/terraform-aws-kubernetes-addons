f39b88f19172f52a35dfbeadd3231f2277273eef
Anh Quach <tuananh.quach@moca.vn>
2022-06-29 14:01:21 +0700

    Remove EBS role

--- iam.tf
+++ iam.tf
@@ -1,42 +1,4 @@
 ###########################################################
-# EBS CSI Addon IAM Role
-###########################################################
-data "aws_iam_policy" "csi_ebs" {
-  name = "AmazonEBSCSIDriverPolicy"
-}
-
-resource "aws_iam_role" "csi_ebs" {
-  name = "AmazonEBSCSIDriverRole"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
-      {
-        Effect : "Allow",
-        Principal : {
-          Federated : "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_provider}"
-        },
-        Action : "sts:AssumeRoleWithWebIdentity",
-        Condition : {
-          StringEquals : {
-            "${var.oidc_provider}:aud" : "sts.amazonaws.com",
-            "${var.oidc_provider}:sub" : "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "csi_ebs" {
-  role       = aws_iam_role.csi_ebs.name
-  policy_arn = data.aws_iam_policy.csi_ebs.arn
-}
-
-
-###########################################################
 # AWS LoadBalancer Controller IAM Role
 ###########################################################
 resource "aws_iam_policy" "aws_loadbalancer_controller" {
