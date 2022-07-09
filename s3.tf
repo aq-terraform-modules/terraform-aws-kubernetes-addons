98b8a8e95be01942f41e473229fdda1340116602
Anh Quach <tuananh.quach@moca.vn>
2022-07-09 13:25:01 +0700

    Update condition for creating s3 velero or not

--- s3.tf
+++ s3.tf
@@ -5,14 +5,15 @@ module "s3_velero_label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
 
-  enabled    = var.enable_efs_csi_driver ? true : false
+  enabled    = var.enable_velero ? true : false
   attributes = ["s3-velero"]
   context    = var.base_label_context
 }
 
 module "s3_velero" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.3.0"
+  source        = "terraform-aws-modules/s3-bucket/aws"
+  version       = "3.3.0"
+  create_bucket = enable_velero ? true : false
 
   bucket = module.s3_velero_label.id
   acl    = "private"
